39e8dd7c18421dc82224787215b598586eb9a02c
deirus <darioivanjurado@gmail.com>
2022-10-19 22:10:49 -0300

    back end

---
+++ ProyectoDario/BackEnd/src/test/java/com/portfolio/jurado/JuradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.jurado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
